7a8fec734fced4cf10fc060fd546b6611b5d6a47
a <cheung.ying.lon@gmail.com>
2018-01-05 13:14:54 -0800

    mobile design for users index

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -3,4 +3,9 @@ module LayoutsHelper
 	def header_type
 		"secondary" unless current_page?(root_path)
 	end
+
+	# style menu background according to pge type
+	def menu_type
+		"secondary" if current_page?(users_path)
+	end
 end
