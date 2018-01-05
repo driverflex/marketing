c260c59025c6510c10a2d5337a1ddd6be3123525
a <cheung.ying.lon@gmail.com>
2018-01-04 20:17:35 -0800

    mobile design for terms and privacy pages

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -1,5 +1,5 @@
 module LayoutsHelper
-	# style background according to page type
+	# style header background according to page type
 	def header_type
 		"secondary" unless current_page?(root_path)
 	end
